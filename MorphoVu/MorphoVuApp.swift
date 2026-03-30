c9fae824f66af3fe691ed85966734baa50d28a11
Sinan Karasu <sinan@bozuk.org>
2026-03-30 13:44:04 -0700

    Initial Commit

---
+++ MorphoVu/MorphoVuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphoVuApp.swift
+//  MorphoVu
+//
+//  Created by Sinan Karasu on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct MorphoVuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
